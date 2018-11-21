ed6ce519d7788a553a02195cbd15a0b011d6aa49
Nattawit Chaiworawitsakul <c.nattawit@gmail.com>
2018-11-21 13:32:37 +0700

    assign path for each api

--- main.go
+++ main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.JSON(http.StatusOK, echo.Map{
-			"message": "pong",
-		})
-	})
+
+	e.GET("/todos", list)
+	e.POST("/todos", create)
+	e.GET("/todos/:id", view)
+	e.PUT("/todos/:id", done)
+	e.DELETE("/todos/:id", remove)
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func list(c echo.Context) error {
+	return nil
+}
+
+func view(c echo.Context) error {
+	return nil
+}
+
+func create(c echo.Context) error {
+	return nil
+}
+
+func done(c echo.Context) error {
+	return nil
+}
+
+func remove(c echo.Context) error {
+	return nil
 }
