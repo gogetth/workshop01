d043697eebe483705212f316b82d5c2531b841e6
Nattawit Chaiworawitsakul <c.nattawit@gmail.com>
2018-11-21 13:44:11 +0700

    implement create api with mgo

--- main.go
+++ main.go
@@ -1,37 +1,82 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
 	"github.com/labstack/echo"
 )
 
 func main() {
+	connString := "root:example@localhost"
+	conn, err := mgo.Dial(connString)
+	if err != nil {
+		log.Printf("dial mongodb server with connection string %q: %v", connString, err)
+		return
+	}
+
+	h := &handler{
+		mongo: conn,
+		db:    "document",
+		col:   "todo",
+	}
+
 	e := echo.New()
 
-	e.GET("/todos", list)
-	e.POST("/todos", create)
-	e.GET("/todos/:id", view)
-	e.PUT("/todos/:id", done)
-	e.DELETE("/todos/:id", remove)
+	e.GET("/todos", h.list)
+	e.POST("/todos", h.create)
+	e.GET("/todos/:id", h.view)
+	e.PUT("/todos/:id", h.done)
+	e.DELETE("/todos/:id", h.remove)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
-func list(c echo.Context) error {
+type todo struct {
+	ID    bson.ObjectId `json:"id" bson:"_id"`
+	Topic string        `json:"topic" bson:"topic"`
+	Done  bool          `json:"done" bson:"done"`
+}
+
+type handler struct {
+	mongo *mgo.Session
+	db    string
+	col   string
+}
+
+func (h *handler) list(c echo.Context) error {
 	return nil
 }
 
-func view(c echo.Context) error {
+func (h *handler) view(c echo.Context) error {
 	return nil
 }
 
-func create(c echo.Context) error {
+func (h *handler) create(c echo.Context) error {
+	id := bson.NewObjectId()
+	var t todo
+	if err := c.Bind(&t); err != nil {
+		return err
+	}
+	t.ID = id
+	t.Done = false
+
+	conn := h.mongo.Copy()
+	defer conn.Close()
+	if err := conn.DB(h.db).C(h.col).Insert(t); err != nil {
+		return err
+	}
+
+	c.JSON(http.StatusOK, t)
 	return nil
 }
 
-func done(c echo.Context) error {
+func (h *handler) done(c echo.Context) error {
 	return nil
 }
 
-func remove(c echo.Context) error {
+func (h *handler) remove(c echo.Context) error {
 	return nil
 }
