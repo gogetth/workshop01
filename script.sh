c5025422509e25b3099a468dcf3ae22b873c599f
Nattawit Chaiworawitsakul <c.nattawit@gmail.com>
2018-11-29 10:35:06 +0700

    add shell command for git deployment

--- script.sh
+++
@@ -1,8 +0,0 @@
-heroku container:login
-heroku create
-heroku config:set MONGO_USER=
-heroku config:set MONGO_PASS=
-heroku config:set MONGO_HOST=
-heroku container:push web
-heroku container:release web
-heroku open
